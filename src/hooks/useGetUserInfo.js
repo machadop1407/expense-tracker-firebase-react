a9a8a7ff3de9ec639fb3a978becf3b23aaaf87e1
Pedro Henrique Ferraz Machado <pedrotech@Pedros-Mac-Studio.local>
2023-08-24 17:24:54 -0700

    fixed

--- src/hooks/useGetUserInfo.js
+++ src/hooks/useGetUserInfo.js
@@ -1,11 +1,6 @@
 export const useGetUserInfo = () => {
-  if (!localStorage.getItem("auth")) {
-    return { name: "", profilePhoto: "", userID: null, isAuth: false };
-  }
-
-  const { name, profilePhoto, userID, isAuth } = JSON.parse(
-    localStorage.getItem("auth")
-  );
+  const { name, profilePhoto, userID, isAuth } =
+    JSON.parse(localStorage.getItem("auth")) || {};
 
   return { name, profilePhoto, userID, isAuth };
 };
